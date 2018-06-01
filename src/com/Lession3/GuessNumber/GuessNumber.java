df621efee23c99a35f824eabd95fdc1bdae49e39
Georgiy-Vwfe <gerrra2000@gmail.com>
2018-06-01 15:35:18 +0300

    create arrays

--- src/com/Lession3/GuessNumber/GuessNumber.java
+++ src/com/Lession3/GuessNumber/GuessNumber.java
@@ -1,46 +1,51 @@
 package com.Lession3.GuessNumber;
+
 import java.util.Random;
 import java.util.Scanner;
 
 public class GuessNumber {
+    private Scanner scanner = new Scanner(System.in);
+    private Player player = new Player();
+    private int findNumber;
+    private int enteredNumber;
+    private int fillAnswersI = 0;
+    private int fillAnswersJ = 0;
+
 
     public void playGame() {
-        Scanner scanner = new Scanner(System.in);
         Random random = new Random();
         String quit = "no";
-        int findNumber;
-        int enteredNumber;
-
-        Player player1 = new Player();
-        Player player2 = new Player();
-        player1.name();
-        player2.name();
-
-        int parity = 0;
-
-        findNumber = random.nextInt(100);
+        player.newName();
+        String player1 = player.initPlayer();
+        String player2 = player.initPlayer();
+        findNumber = random.nextInt(1);
         while (quit.equals("no")) {
-            if (parity % 2 == 0) {
-                System.out.println("Your turn, " + player1.getPlayer());
-                parity++;
-            } else {
-                System.out.println("Your turn, " + player2.getPlayer());
-                parity++;
-            }
+            System.out.println("Your turn, " + player.getPlayer());
             System.out.print("Enter num: ");
             enteredNumber = scanner.nextInt();
-
+            player.fillAnswer();
             if (enteredNumber == findNumber) {
                 System.out.println("Entered num = hidden num");
+                if (player.getParity() % 2 == 0) {
+                    System.out.println("\nPlayer " + player1 + " find number " + findNumber + " in " + player.getNumberOfMoves() + " moves\n");
+                } else if (player.getParity() % 2 == 1) {
+                    System.out.println("\nPlayer " + player2 + " find number " + findNumber + " in " + player.getNumberOfMoves() + " moves\n");
+                } else {
+                    System.out.println("\nError\n");
+                }
                 System.out.print("Do you want quit?\n" + "Type yes or no: ");
                 scanner.nextLine();
                 quit = scanner.nextLine();
+                if (quit.equals("no")) {
+                    player.setParity(0);
+                    player.setNumberOfMovesI(0);
+                    player.setNumberOfMovesJ(0);
+                }
             } else if (enteredNumber > findNumber) {
                 System.out.println("Entered num > hidden num");
             } else if (enteredNumber < findNumber) {
                 System.out.println("Entered num < hidden num");
             }
-
         }
     }
 
--- src/com/Lession3/GuessNumber/Player.java
+++ src/com/Lession3/GuessNumber/Player.java
@@ -1,17 +1,68 @@
 package com.Lession3.GuessNumber;
+
 import java.util.Scanner;
 
 public class Player {
-    private String player;
+    private String[] players = new String[2];
+    private int parity = 0;
+    private int numberOfMovesI = 0;
+    private int numberOfMovesJ = 0;
 
-
-    public void name() {
+    public void newName() {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Print your name, Player: ");
-        player = scanner.nextLine();
+        for (int i = 0; i < 2; i++) {
+            System.out.print("Print your name, Player: ");
+            players[i] = scanner.nextLine();
+        }
+    }
+
+    public String initPlayer() {
+        if (parity % 2 == 0) {
+            return players[0];
+        } else {
+            return players[1];
+        }
     }
 
     public String getPlayer() {
-        return player;
+        if (parity % 2 == 0) {
+            parity++;
+            return players[0];
+        } else if (parity % 2 == 1){
+            parity++;
+            return players[1];
+        } else return null;
+    }
+
+    public void fillAnswer() {
+        if (parity % 2 == 0) {
+            numberOfMovesI++;
+        } else if (parity % 2 == 1) {
+            numberOfMovesJ++;
+        } else return;
+    }
+
+    public int getNumberOfMoves(){
+        if (parity % 2 == 0) {
+            return numberOfMovesI;
+        } else if (parity % 2 == 1) {
+            return numberOfMovesJ;
+        } else return 1;
+    }
+
+    public int getParity() {
+        return parity;
+    }
+
+    public void setParity(int parity) {
+        this.parity = parity;
+    }
+
+    public void setNumberOfMovesI(int numberOfMovesI) {
+        this.numberOfMovesI = numberOfMovesI;
+    }
+
+    public void setNumberOfMovesJ(int numberOfMovesJ) {
+        this.numberOfMovesJ = numberOfMovesJ;
     }
 }
