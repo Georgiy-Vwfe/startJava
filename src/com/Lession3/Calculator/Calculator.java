f0c5620ebc5eaf833a56186f8f5633746a242d03
Georgiy-Vwfe <gerrra2000@gmail.com>
2018-06-02 12:34:06 +0300

    Create history

--- src/com/Lession3/Calculator/Calculator.java
+++ src/com/Lession3/Calculator/Calculator.java
@@ -1,27 +1,27 @@
 package com.Lession3.Calculator;
+
 import java.util.Scanner;
 
 public class Calculator {
 
     private Scanner scanner = new Scanner(System.in);
     private String wantContinue = "yes";
-    private double x;
-    private double y;
     private byte selector;
+    private String[] history = new String[100];
+    private String result;
+    private int i = 0;
 
     void continueCalc() {
-        System.out.print("Do you want Continue?\nEnter yes or no: ");
+        System.out.print("\nDo you want Continue?\nEnter yes or no: ");
         scanner.nextLine();
         wantContinue = scanner.nextLine();
     }
 
     void run() {
+        double x;
+        double y;
         System.out.println("Hello there! It's calculator.");
         while (wantContinue.equals("yes")) {
-            System.out.print("Enter x: ");
-            x = scanner.nextDouble();
-            System.out.println("Enter y: ");
-            y = scanner.nextDouble();
             System.out.println("What you want?");
             System.out.println("1. summation\n" +
                     "2. subtraction\n" +
@@ -29,42 +29,85 @@ public class Calculator {
                     "4. division\n" +
                     "5. exponentiation\n" +
                     "6. mod\n" +
+                    "7. History\n" +
                     "0. Quit");
             selector = scanner.nextByte();
             switch (selector) {
-                case 1: {
-                    System.out.println("x + y = " + (x + y));
+                case 1:
+                    System.out.print("Enter x: ");
+                    x = scanner.nextDouble();
+                    System.out.print("Enter y: ");
+                    y = scanner.nextDouble();
+                    result = x + " + " + y + " = " + (x + y);
+                    System.out.print(result);
+                    fillHistory();
                     break;
-                }
-                case 2: {
-                    System.out.println("x - y = " + (x - y));
+                case 2:
+                    System.out.print("Enter x: ");
+                    x = scanner.nextDouble();
+                    System.out.print("Enter y: ");
+                    y = scanner.nextDouble();
+                    result = x + " - " + y + " = " + (x - y);
+                    System.out.print(result);
+                    fillHistory();
                     break;
-                }
-                case 3: {
-                    System.out.println("x * y = " + (x * y));
+                case 3:
+                    System.out.print("Enter x: ");
+                    x = scanner.nextDouble();
+                    System.out.print("Enter y: ");
+                    y = scanner.nextDouble();
+                    result = x + " * " + y + " = " + (x * y);
+                    System.out.print(result);
+                    fillHistory();
                     break;
-                }
-                case 4: {
-                    System.out.println("x / y = " + (x / y));
+                case 4:
+                    System.out.print("Enter x: ");
+                    x = scanner.nextDouble();
+                    System.out.print("Enter y: ");
+                    y = scanner.nextDouble();
+                    result = x + " / " + y + " = " + (x / y);
+                    System.out.print(result);
+                    fillHistory();
                     break;
-                }
-                case 5: {
-                    System.out.println("x^y = " + Math.pow(x, y));
+                case 5:
+                    System.out.print("Enter x: ");
+                    x = scanner.nextDouble();
+                    System.out.print("Enter y: ");
+                    y = scanner.nextDouble();
+                    result = x + "^" + y + " = " + Math.pow(x, y);
+                    System.out.print(result);
+                    fillHistory();
                     break;
-                }
-                case 6: {
-                    System.out.println("x mod y = " + x % y);
+                case 6:
+                    System.out.print("Enter x: ");
+                    x = scanner.nextDouble();
+                    System.out.print("Enter y: ");
+                    y = scanner.nextDouble();
+                    result = x + " mod " + y + " = " + (x % y);
+                    System.out.print(result);
+                    fillHistory();
                     break;
-                }
-                case 0: {
+                case 7:
+                    System.out.println("History:");
+                    System.out.println("---------------------------------");
+                    for (i = 0; i < 100; i++) {
+                        if (history[i] != null) {
+                            System.out.println(history[i]);
+                        }
+                    }
+                    break;
+                case 0:
                     wantContinue = "no";
                     break;
-                }
-                default: {
+                default:
                     System.out.println("Error number.");
-                }
             }
             continueCalc();
         }
+    }
+
+    private void fillHistory() {
+        history[i] = result;
+        i++;
     }
 }
