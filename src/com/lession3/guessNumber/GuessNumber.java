4d1b6f389cbe065884af662d27c3ff292fb1dd93
Georgiy-Vwfe <gerrra2000@gmail.com>
2018-06-04 17:59:13 +0300

    update GuessNumber

--- src/com/lession3/guessNumber/GuessNumber.java
+++ src/com/lession3/guessNumber/GuessNumber.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 
 public class GuessNumber {
     private Scanner scanner = new Scanner(System.in);
-    private String[] players;
     private byte parity = 0;
     private int findNumber;
     private int enteredNumber;
@@ -13,99 +12,120 @@ public class GuessNumber {
     private int[] Answers = new int[100];
     private int numberOfPlayers;
     private int[] numberOfMoves;
+    String quit = "";
+    Player player0 = new Player();
+    Player player1 = new Player();
+    Player player2 = new Player();
 
-    private String[] newName() {
+    private void initPlayers() {
         do {
             System.out.println("How many players will play?");
             System.out.println("Attention! The number of players can't be more than 2");
             numberOfPlayers = scanner.nextInt();
-        } while (numberOfPlayers <= 0 || numberOfPlayers > 2);
+        } while (numberOfPlayers < 1 || numberOfPlayers > 2);
         numberOfMoves = new int[numberOfPlayers];
-        for (int i = 0; i < numberOfPlayers; i++) {
-            System.out.print("Print your name, Player №" + (i + 1) + ": ");
-            players[i] = scanner.nextLine();
-        }
-        return players;
-    }
-
-    private void initPlayers() {
-        switch (newName().length) {
+        switch (numberOfPlayers) {
             case 1:
-                Player player1 = new Player();
-                player1.setPlayerName(players[1]);
+                System.out.print("Print your name, Player №1: ");
+                scanner.nextLine();
+                player0.setPlayerName(scanner.nextLine());
                 break;
             case 2:
-                Player player2 = new Player();
-                Player player3 = new Player();
-                player2.setPlayerName(players[1]);
-                player3.setPlayerName(players[2]);
+                System.out.print("Print your name, Player №1: ");
+                scanner.nextLine();
+                player1.setPlayerName(scanner.nextLine());
+                System.out.print("Print your name, Player №2: ");
+                player2.setPlayerName(scanner.nextLine());
                 break;
         }
     }
 
     public void playGame() {
         Random random = new Random();
-        String quit = "";
-        newName();
         initPlayers();
         findNumber = random.nextInt(1);
-        while (!quit.equals("yes")) {
-            System.out.println("Your turn, " + playerList());
-            parity++;
-            System.out.print("Enter num: ");
-            enteredNumber = scanner.nextInt();
-            fillAnswers();
-            numberOfMovesCounter();
-            if (enteredNumber == findNumber) {
-                System.out.println("Entered num = hidden num");
-                returnAnswers();
-                System.out.println("\nPlayer " + playerList() + " find number " + findNumber + " in " + numberOfMoves[parity % 2] + " moves\n");
-        System.out.print("Do you want quit?\n" + "Type yes or no: ");
-        scanner.nextLine();
-        quit = scanner.nextLine();
-        if (!quit.equals("yes")) {
-        parity = 0;
-        numberOfMoves[0] = 0;
-        numberOfMoves[1] = 0;
-        }
-        } else if (enteredNumber > findNumber) {
-        System.out.println("Entered num > hidden num");
-        } else if (enteredNumber < findNumber) {
-        System.out.println("Entered num < hidden num");
-        }
-        }
+        switch (numberOfPlayers) {
+            case 1:
+                while (!quit.equals("yes")) {
+                    System.out.println("Your turn, " + player0.getPlayerName());
+                    System.out.print("Enter num: ");
+                    enteredNumber = scanner.nextInt();
+                    fillAnswers();
+                    numberOfMovesCounter();
+                    result();
+                }
+                break;
+            case 2:
+                while (!quit.equals("yes")) {
+                    System.out.println("Your turn, " + playerList());
+                    parity++;
+                    System.out.print("Enter num: ");
+                    enteredNumber = scanner.nextInt();
+                    fillAnswers();
+                    numberOfMovesCounter();
+                    result();
+                }
+                break;
         }
 
-public void fillAnswers() {
+    }
+
+
+    private void fillAnswers() {
         Answers[countAnswer] = enteredNumber;
         countAnswer++;
-        }
+    }
 
-public void returnAnswers() {
+    private void returnAnswers() {
         System.out.print("Answers of player " + playerList() + ":");
         if (parity % 2 == 0) {
-        for (int j = 0; j < 100; j += 2) {
-        if (Answers[j] == 0) {
-        return;
-        }
-        System.out.print(" " + Answers[j]);
-        }
+            for (int j = 0; j < 100; j += 2) {
+                if (Answers[j] == 0) {
+                    return;
+                }
+                System.out.print(" " + Answers[j]);
+            }
         } else if (parity % 2 == 1) {
-        for (int j = 1; j < 100; j += 2) {
-        if (Answers[j] == 0) {
-        return;
-        }
-        System.out.print(" " + Answers[j]);
-        }
+            for (int j = 1; j < 100; j += 2) {
+                if (Answers[j] == 0) {
+                    return;
+                }
+                System.out.print(" " + Answers[j]);
+            }
         } else return;
+    }
+
+
+    private String playerList() {
+        if (parity%2 == 0){
+            return player1.getPlayerName();
+        } else if (parity%2 ==1){
+            return player2.getPlayerName();
         }
+        return "Error";
+    }
 
-
-public String playerList() {
-        return players[parity % 2];
-        }
-
-public void numberOfMovesCounter() {
+    private void numberOfMovesCounter() {
         numberOfMoves[parity % 2]++;
+    }
+    private void result(){
+        if (enteredNumber == findNumber) {
+            System.out.println("Entered num = hidden num");
+            returnAnswers();
+            System.out.println("\nPlayer " + player0.getPlayerName() + " find number " + findNumber + " in " + numberOfMoves[parity % 2] + " moves\n");
+            System.out.print("Do you want quit?\n" + "Type yes or no: ");
+            scanner.nextLine();
+            wantExit();
+        } else if (enteredNumber > findNumber) {
+            System.out.println("Entered num > hidden num");
+        } else if (enteredNumber < findNumber) {
+            System.out.println("Entered num < hidden num");
         }
+    }
+    private void wantExit(){
+        quit = scanner.nextLine();
+        if (!quit.equals("yes")) {
+            numberOfMoves[0] = 0;
         }
+    }
+}
--- src/com/lession3/guessNumber/Player.java
+++ src/com/lession3/guessNumber/Player.java
@@ -9,4 +9,8 @@ public class Player {
     public void setPlayerName(String playerName) {
         this.playerName = playerName;
     }
+
+    public String getPlayerName() {
+        return playerName;
+    }
 }
